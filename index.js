841bdc805db6fc87ea5306a2573f26ee225973ee
John Otander <johnotander@gmail.com>
2015-09-23 11:56:16 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function doNotDisturb(options) {
+  options = options || {};
+
+  return true;
+};
---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var doNotDisturb = require('..');
+
+describe('do-not-disturb', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(doNotDisturb(), true);
+  });
+});
