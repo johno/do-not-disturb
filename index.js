fe492b5e49ede6b4f24e18ed66e905f74a456b33
Richard Wu <rwu@palantir.com>
2017-03-30 10:37:02 -0700

    fix script to work with OS Sierra

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 'use strict';
 const runApplescript = require('run-applescript');
+const os = require('os');
+
+function getMenuBarId() {
+  const release = os.release().split(".");
+  const osSierraVersion = 16;
+  if (release.length > 0 && parseInt(release[0]) >= osSierraVersion) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
 
 module.exports.on = function () {
-  if (process.platform !== 'darwin') {
+  if (os.platform() !== 'darwin') {
     throw new Error('do-not-disturb only supports the darwin platform');
   }
+  const menuBarId = getMenuBarId();
 
   return runApplescript(`
     tell application "System Events"
       tell application process "SystemUIServer"
         try
-					if not (exists menu bar item "Notification Center, Do Not Disturb enabled" of menu bar 2 of application process "SystemUIServer" of application "System Events") then
+					if not (exists menu bar item "Notification Center, Do Not Disturb enabled" of menu bar ${menuBarId} of application process "SystemUIServer" of application "System Events") then
             key down option
-            click menu bar item "Notification Center" of menu bar 2
+            click menu bar item "Notification Center" of menu bar ${menuBarId}
             key up option
           end if
         on error
@@ -23,17 +35,18 @@ module.exports.on = function () {
 };
 
 module.exports.off = function () {
-  if (process.platform !== 'darwin') {
+  if (os.platform() !== 'darwin') {
     throw new Error('do-not-disturb only supports the darwin platform');
   }
+  const menuBarId = getMenuBarId();
 
   return runApplescript(`
     tell application "System Events"
       tell application process "SystemUIServer"
         try
-          if exists menu bar item "Notification Center, Do Not Disturb enabled" of menu bar 2 of application process "SystemUIServer" of application "System Events" then
+          if exists menu bar item "Notification Center, Do Not Disturb enabled" of menu bar ${menuBarId} of application process "SystemUIServer" of application "System Events" then
             key down option
-            click menu bar item "Notification Center, Do Not Disturb enabled" of menu bar 2
+            click menu bar item "Notification Center, Do Not Disturb enabled" of menu bar ${menuBarId}
             key up option
           end if
         on error
